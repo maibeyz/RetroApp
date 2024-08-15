33c4bba94c9048e46a69cd652ab41b8ff6b14b1d
maibeyz <maidebeyzabulbul@gmail.com>
2024-08-15 13:26:13 +0300

    UI and logo modified

--- app/src/main/java/com/mai/retroapp/ui/viewmodel/JoinAnonymus.kt
+++ app/src/main/java/com/mai/retroapp/ui/viewmodel/JoinAnonymus.kt
@@ -10,6 +10,7 @@ import android.widget.Switch
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.auth.ktx.auth
